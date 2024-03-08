30aace39762601d694bbd55be965422f721040d0
timberlinecs <chinmaytanmay@outlook.com>
2024-03-08 13:17:07 -0700

    multuthread??

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <filesystem>
 #include <set>
+#include <thread>
 
 #include <vfxtools.h>
 
+int finished1, finished2, finished3, finished4, finished5;
 
+//int done = 0;
 namespace fs = std::filesystem;
+
+VfxTools tool;
+
+void batchRun(int start, int end, std::vector<Frame*> frames, int* finished){
+    
+    for(int i  = start + 1; i < end; i++){
+        *finished = (int)(((i - start) / (float)(end - start)) * 100.0f);
+        //printf("(%d %d) | Percent done: %d\n", start, end, (int)((done / (float)frames.size()) * 100.0f));
+        //printf("Thead (%d %d) is %d done\n", start, end, (int)(((float)i/(float)end) * 100.0f));
+        tool.OpticalFlow(frames[i - 1], frames[i]);
+        //done++;
+    }
+    *finished = (int)((end / (float)end) * 100.0f);
+    printf("Thread (%d %d) finished!\n", start, end);
+    return;
+}
 
 int main(int args, char** argv){
     if(args < 3){
         printf("Not enough args.\n");
         return -1;
     }
-    VfxTools vfxtools;
+    
 
     /*  
         Arg fornmat:
@@ -39,11 +58,51 @@ int main(int args, char** argv){
     system("rm CONVS/*");
     system("rm CONVSFULL/*");
     printf("\n");
-    for(int i = 1; i < frames.size(); i++){
+
+    int batches = frames.size() / 5;
+    int start = 0;
+
+    std::thread bat1(batchRun, start, start + batches - 1, frames, &finished1);
+    start += batches;
+    
+    std::thread bat2(batchRun, start, start + batches - 1, frames, &finished2);
+    start += batches;
+
+    std::thread bat3(batchRun, start, start + batches - 1, frames, &finished3);
+    start += batches;
+
+    std::thread bat4(batchRun, start, start + batches - 1, frames, &finished4);
+    start += batches;
+
+    std::thread bat5(batchRun, start, start + batches - 1, frames, &finished5);
+    start += batches;
+
+    
+
+    printf("\n\n\n");
+
+    while(1){
+        printf("==== PROGRESS ====\n");
+        printf("Thread 1 | %d %   \n", finished1);
+        printf("Thread 2 | %d %  \n", finished2);
+        printf("Thread 3 | %d %  \n", finished3);
+        printf("Thread 4 | %d %  \n", finished4);
+        printf("Thread 5 | %d %  \n", finished5);
+        printf("\x1b[6A");
+    }
+
+    bat5.join();
+    bat3.join();
+    bat1.join();
+    bat2.join();
+    bat4.join();
+    
+    
+
+    /*for(int i = 1; i < frames.size(); i++){
         printf("Percent Complete: %d | Frame %s\n", (int)((i/ (float)frames.size()) * 100.0f), frames[i]->getPath().c_str());
         vfxtools.OpticalFlow(frames[i - 1], frames[i]);
-        delete(frames[i-1]);
-    }
+    }*/
     system("cp T1/* COMBINED");
     system("cp OUT/* COMBINED");
     return 0;
--- src/tools/opticalflow.cpp
+++ src/tools/opticalflow.cpp
@@ -54,7 +54,7 @@ void OpticalFlow::OpticFlowMain(Frame* f1, Frame* f2){
     Frame grayf1("frame1G.png", f1->getWidth(), f1->getHeight());
     Frame grayf2("frame3G.png", f1->getWidth(), f1->getHeight());
 
-    Frame intermediate("INTER/" + std::to_string(num),f1->getWidth(), f1->getHeight());
+    //Frame intermediate("INTER/" + std::to_string(num),f1->getWidth(), f1->getHeight());
 
     for(int y = 0; y < f1->getHeight(); y++){
         PixelRow row1 = *(f1->getRow(y));
@@ -78,8 +78,8 @@ void OpticalFlow::OpticFlowMain(Frame* f1, Frame* f2){
 
     Frame dervY = Math::conv(grayf1, sobelY);
     Frame dervX = Math::conv(grayf1, sobelX);
-    dervY.Write();
-    dervX.Write();
+    //dervY.Write();
+    //dervX.Write();
 
     Frame derv("CONVSFULL/" + std::to_string(num), dervY.getWidth(), dervX.getHeight());
 
@@ -95,59 +95,19 @@ void OpticalFlow::OpticFlowMain(Frame* f1, Frame* f2){
         }
         derv.setRow(rowDY, y);
     }
-
-
     derv.Write();
-
-    //grayf1.Write();
-    //grayf2.Write();
-
-    /*
-        I(x, y, t) = (x + deltaX, y + deltaY, t + deltaT)
-    */
-    for(int y = 0; y < f1->getHeight(); y++){
-        PixelRow row1 = *(f1->getRow(y));
-        PixelRow row2 = *(f2->getRow(y));
-        for(int x = 1; x < f1->getWidth() - 1; x++){
-            Pixel tmp;
-            float I = (row2[x].r - row1[x].r);
-            tmp.r = (1/I);
-            tmp.b = (1/I);
-            tmp.g = (1/I);
-            row1.setPixel(tmp, x);
-        }
-        intermediate.setRow(row1, y);
-    }
     
-    intermediate.Write();
-
-    /*for(int y = 0; y < f1->getHeight(); y++){
-        PixelRow row1 = *(f1->getRow(y));
-        PixelRow row2 = *(f2->getRow(y));
-        PixelRow rowDY = *(dervY.getRow(y));
-        PixelRow rowDX = *(dervX.getRow(y));
-        PixelRow replace = *(greenScreenReplace.getRow(y));
-        for(int x = 1; x < f1->getWidth() - 1; x++){
-            Pixel tmp;
-            float thing = ((((rowDY[x].r) / 255.0f)) + ((rowDY[x].g) / 255.0f) + ((rowDY[x].b) / 255.0f)) / 3;
-            tmp.r = ((row2[x].r / thing) + (row1[x].r)) / 2;
-            tmp.g = ((row2[x].g / thing) + (row1[x].g)) / 2;
-            tmp.b = ((row2[x].b / thing) + (row1[x].b)) / 2;
-            row1.setPixel(tmp, x);
-        }
-        out.setRow(row1, y);
-    }*/
     for(int y = 0; y < f1->getHeight(); y++){
-        PixelRow row1 = *(f1->getRow(y));
-        PixelRow row2 = *(f2->getRow(y));
+        PixelRow row1 = *(grayf1.getRow(y));
+        PixelRow row2 = *(grayf2.getRow(y));
         PixelRow rowDY = *(dervY.getRow(y));
         PixelRow rowDX = *(dervX.getRow(y));
         for(int x = 0; x < f1->getWidth(); x++){
             Pixel tmp;
-            float iX = rowDX[x].r;
-            float iY = rowDY[x].r;
+            float iX = rowDX[x].r * rowDX[x].r;
+            float iY = rowDY[x].r * rowDY[x].r;
             float iXY = iX * iY;
-            float iT = 0.4;
+            float iT = (row2[x].r - row1[x].r) / 2;
             float deter = 1 / ((iX * iY) - (iXY * iXY));
 
             float inv1 = deter * iX;
@@ -159,9 +119,9 @@ void OpticalFlow::OpticFlowMain(Frame* f1, Frame* f2){
 
             float mag = sqrt((vX * vX) + (vY * vY));
 
-            tmp.r = mag;
-            tmp.g = mag;
-            tmp.b = mag;
+            tmp.r = (mag * 6 + row1[x].r) / 7;
+            tmp.g = (mag * 6 + row1[x].g) / 7;
+            tmp.b = (mag * 6 + row1[x].b) / 7;
             
 
             row1.setPixel(tmp, x);
@@ -170,4 +130,5 @@ void OpticalFlow::OpticFlowMain(Frame* f1, Frame* f2){
     }
 
     out.Write();
+    delete(f1);
 }
